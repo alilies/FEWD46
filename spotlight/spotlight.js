d0bc2347a7a4b6443c462b7378d205970720f4e5
Elisa li <ilasile@gmail.com>
2016-07-28 19:59:59 -0700

    fixing intervals in spotlight

--- spotlight/spotlight.js
+++ spotlight/spotlight.js
@@ -2,30 +2,34 @@ $(".stop").on("click", redLight);
 $(".slow").on("click", yellowLight);
 $(".go").on("click", greenLight);
 $(".caution").on("click", blinkingLight);
+var interval;
 
 function redLight() {
+    clearInterval(interval);
     $(".red-circle").toggleClass("red-light");
     $(".yellow-circle").removeClass("yellow-light");
     $(".green-circle").removeClass("green-light");
 }
 
 function yellowLight() {
+    clearInterval(interval);
     $(".red-circle").removeClass("red-light");
     $(".green-circle").removeClass("green-light");
     $(".yellow-circle").toggleClass("yellow-light");
 }
 
 function greenLight() {
+    clearInterval(interval);
     $(".red-circle").removeClass("red-light");
     $(".yellow-circle").removeClass("yellow-light");
     $(".green-circle").toggleClass("green-light");
 }
 
 function blinkingLight() {
-  setInterval(flash, 1000);
+  interval = setInterval(flash, 1000);
   function flash() {
-  $(".red-circle").removeClass("red-light");
-  $(".green-circle").removeClass("green-light");
-  $(".yellow-circle").toggleClass("yellow-light");
+    $(".red-circle").removeClass("red-light");
+    $(".green-circle").removeClass("green-light");
+    $(".yellow-circle").toggleClass("yellow-light");
   }
 }
