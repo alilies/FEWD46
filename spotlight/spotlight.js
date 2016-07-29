8b3cd0379bae66ec849732b22dd9c9c0b89b741b
Elisa li <ilasile@gmail.com>
2016-07-28 18:34:02 -0700

    spotlight

--- spotlight/spotlight.js
+++ spotlight/spotlight.js
@@ -1,3 +1,31 @@
-// When you click stop, circle-1 adds red class
-// When you click slow,  circle-2 adds yellow blinking class
-// When you click go, circle-3 adds then green class
+$(".stop").on("click", redLight);
+$(".slow").on("click", yellowLight);
+$(".go").on("click", greenLight);
+$(".caution").on("click", blinkingLight);
+
+function redLight() {
+    $(".red-circle").toggleClass("red-light");
+    $(".yellow-circle").removeClass("yellow-light");
+    $(".green-circle").removeClass("green-light");
+}
+
+function yellowLight() {
+    $(".red-circle").removeClass("red-light");
+    $(".green-circle").removeClass("green-light");
+    $(".yellow-circle").toggleClass("yellow-light");
+}
+
+function greenLight() {
+    $(".red-circle").removeClass("red-light");
+    $(".yellow-circle").removeClass("yellow-light");
+    $(".green-circle").toggleClass("green-light");
+}
+
+function blinkingLight() {
+  setInterval(flash, 1000);
+  function flash() {
+  $(".yellow-circle").removeClass("red-light");
+  $(".yellow-circle").removeClass("green-light");
+  $(".yellow-circle").toggleClass("yellow-light");
+  }
+}
